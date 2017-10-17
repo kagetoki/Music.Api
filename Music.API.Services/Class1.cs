bce8cea302d8fbe86f92ac489f23225cb12d3852
kagetoki <liman.rom@gmail.com>
2017-10-18 00:49:08 +0300

    initial create

---
+++ Music.API.Services/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Music.API.Services
+{
+
+    public class Class1
+    {
+        void T()
+        {
+            var system = Akka.Actor.ActorSystem.Create("ma");
+            
+        }
+    }
+}
