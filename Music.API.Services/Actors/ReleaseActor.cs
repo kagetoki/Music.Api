9f0e350cb3836258b9af7148ed11f1dab37af02b
kagetoki <liman.rom@gmail.com>
2017-10-19 11:45:19 +0300

    Release Actors init create

---
+++ Music.API.Services/Actors/ReleaseActor.cs
@@ -0,0 +1,49 @@
+﻿using Akka.Actor;
+using Akka.Persistence;
+using Music.API.Interface.Messages;
+using Music.API.Interface.States;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Music.API.Services.Actors
+{
+    public class ReleaseActor : ReceivePersistentActor
+    {
+        private Release _state;
+        public ReleaseActor()
+        {
+            Command<ReleaseUpdateMessage>(msg => HandleUpdateMessage(msg));
+            Recover<ReleaseUpdateMessage>(msg => HandleUpdateMessage(msg));
+        }
+
+        public override string PersistenceId => _state.ReleaseId;
+        private void HandleUpdateMessage(ReleaseUpdateMessage msg)
+        {
+            if (IsMessageValid(msg))
+            {
+                Persist(msg, m => 
+                {
+                    //update state
+
+                    //should I store here to db or it's automatic?
+                });
+            }
+        }
+
+        private bool IsMessageValid(ReleaseUpdateMessage msg)
+        {
+            if(msg == null || msg.ReleaseId != PersistenceId)
+            { return false; }
+            if(_state.Timestamp >= msg.Timestamp)
+            {
+                return false;
+            }
+            //Something has to be updated, otherwise nothing changes and we don't need this message
+            return !string.IsNullOrEmpty(msg.Title)
+                    || !string.IsNullOrEmpty(msg.Genre)
+                    || !string.IsNullOrEmpty(msg.Artist)
+                    || msg.Cover != null;
+        }
+    }
+}
---
+++ Music.API.Services/Actors/ReleaseCreatorActor.cs
@@ -0,0 +1,13 @@
+﻿using Akka.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Music.API.Services.Actors
+{
+    public class ReleaseCreatorActor : ReceivePersistentActor
+    {
+        private long _nextReleaseId;
+        public override string PersistenceId => "release";
+    }
+}
