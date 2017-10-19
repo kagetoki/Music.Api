12fed811bf564a3db142623aed38efe87f061106
kagetoki <liman.rom@gmail.com>
2017-10-19 15:25:09 +0300

    release creator actor update

--- Music.API.Services/Actors/ReleaseCreatorActor.cs
+++ Music.API.Services/Actors/ReleaseCreatorActor.cs
@@ -1,4 +1,6 @@
-﻿using Akka.Persistence;
+﻿using Akka.Actor;
+using Akka.Persistence;
+using Music.API.Interface.Messages;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +11,29 @@ namespace Music.API.Services.Actors
     {
         private long _nextReleaseId;
         public override string PersistenceId => "release";
+
+        public ReleaseCreatorActor()
+        {
+            Command<ReleaseCreateMessage>(msg => HandleCreateMessage(msg));
+        }
+
+        private bool HandleCreateMessage(ReleaseCreateMessage msg)
+        {
+            if (!IsMessageValid(msg))
+            {
+                return false;
+            }
+            //Pass some Props with info of creation and id. Maybe generate initial state of actor
+            //combining info from creation and _nextReleaseId
+            Context.ActorOf<ReleaseActor>();
+            throw new NotImplementedException();
+        }
+
+        private bool IsMessageValid(ReleaseCreateMessage msg)
+        {
+            return msg != null && !string.IsNullOrEmpty(msg.Artist)
+                               && !string.IsNullOrEmpty(msg.Genre)
+                               && !string.IsNullOrEmpty(msg.Title);
+        }
     }
 }
