12e7755bd1304762a8b820a97e2e2f3beaa03831
kagetoki <liman.rom@gmail.com>
2017-10-27 20:54:07 +0300

    subscription command and state init create

---
+++ Music.API.Entities/Commands/SubscriptionCreateCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Music.API.Entities.Commands
+{
+    public class SubscriptionCreateCommand : Command
+    {
+        public string ReleaseId { get; private set; }
+    }
+}
--- Music.API.Entities/States/SubscriptionState.cs
+++ Music.API.Entities/States/SubscriptionState.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Text;
 
 namespace Music.API.Entities.States
 {
     public class SubscriptionState : State
     {
+        public string SubscriptionId { get; private set; }
+        public string ReleaseId { get; private set; }
+        public DateTime UtcExpiration { get; private set; }
+        public ImmutableList<string> ShopIds { get; private set; }
+
+        public SubscriptionState(string subscriptionId, DateTime utcExpiration, string releaseId, ImmutableList<string> shopIds)
+        {
+            SubscriptionId = subscriptionId;
+            UtcExpiration = utcExpiration;
+            ReleaseId = releaseId;
+            ShopIds = shopIds;
+        }
+
+
     }
 }
